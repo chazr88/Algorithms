33f419d1579049ff75a5185e1f29e6832ec42349
Chaz Rowe <chazr_88@hotmail.com>
2020-02-06 10:36:53 -0500

    finished eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,30 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+#O(3^n)
 def eating_cookies(n, cache=None):
-  pass
+  #base case
+  #we found a path
+  if n == 0:
+    return 1
+
+  #we didn't find a path
+  if n < 0:
+    return 0
+
+  #Check the Cache
+  elif cache and cache[n] > 0:
+    return cache[n]
+
+  #recursive case
+  else:
+    if not cache:
+      #create dictionary
+      cache = {i : 0 for i in range (n+1)} # {n:num_ways_to_eat}
+
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
@@ -14,3 +36,6 @@ if __name__ == "__main__":
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+
+
+
